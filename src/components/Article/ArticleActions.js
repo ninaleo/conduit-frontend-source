6999a403da64d800c1063b2a073e3c2dbb6f9280
AB5126 <AB5126@student.jamk.fi>
2021-11-11 10:11:57 +0200

    fixed: delete article and modify article more visible

--- src/components/Article/ArticleActions.js
+++ src/components/Article/ArticleActions.js
@@ -24,7 +24,7 @@ const ArticleActions = props => {
           <i className="ion-edit"></i> Edit Article
         </Link>
 
-        <button className="btn btn-outline-danger btn-sm" onClick={del}>
+        <button className="btn btn-outline-secondary btn-sm" onClick={del}>
           <i className="ion-trash-a"></i> Delete Article
         </button>
 
