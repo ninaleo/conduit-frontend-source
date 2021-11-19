b44b21210d7106eb40ac080a9d78a9e9776e0426
Petri Huhtanen <AB5149@student.jamk.fi>
2021-11-19 12:50:07 +0200

    Add GDPR PDF & functionality to open it in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import GdprPdf from '../documents/WIMMAGDPR.pdf';
 
 const Footer = props => {
 
     return (
 <footer>
 <div className="footer-link">
-<a href="gdpr">GDPR info</a>
+<a href={GdprPdf} target="_blank">GDPR info</a>
 </div>
 <div className="footer-conent">
 <img src="./logo/logo_square.png" alt = "logo" width="250" height="250"/>
